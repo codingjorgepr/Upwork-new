b9f3670ebb8a498da2a24d8d27f77d19c777d9c3
Jorge <113088659+codingjorgepr@users.noreply.github.com>
2023-02-07 09:54:06 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Upwork-new
+# UpworkProject
 Created with CodeSandbox
---
+++ src/jobs.js
@@ -0,0 +1,60 @@
+const JOBS = [
+  {
+    id: 2,
+    title: "React Developer",
+    location: "WorldWide",
+    remote: true,
+    paymentVerified: true,
+    hourlyUSD: 40,
+    postedTime: "4 days ago",
+    description: `Developing new user-facing features using React.js
+    Building reusable components and front-end libraries for future use
+    Translating designs and wireframes into high quality code
+    Optimizing components for maximum performance across a vast array of web-capable devices and browsers
+    Participate in daily stand-ups, bi-weekly sprint planning, and sprint retrospectives for the team`,
+    category: "Front End Dev"
+  },
+  {
+    id: 3,
+    title: "Graphic Designer",
+    location: "United States",
+    remote: true,
+    paymentVerified: true,
+    hourlyUSD: 30,
+    postedTime: "Yesterday",
+    description: `Gather and prepare graphic design project details
+    Design a rough layout of design concepts and present ideas
+    Obtain approval and feedback on design concepts from the Creative Director and other stakeholders
+    Implement feedback and prepare the final copy
+    Deliver the final product in the preferred medium (electronic, print)
+    Maintain technical and creative design knowledge by attending workshops and researching the latest industry trends.`,
+    category: "Design"
+  },
+  {
+    id: 4,
+    title: "UX/UI Designer",
+    location: "United States",
+    remote: true,
+    paymentVerified: true,
+    hourlyUSD: 27,
+    postedTime: "Yesterday",
+    description: `UX/UI Designers are responsible for conceiving and conducting user research, developing articulated ideas and translating them into sitemaps, user flows, customer journey maps, wireframes, mockups and prototypes and finally design each interaction of a user’s digital experience. They are ultimately responsible for the usability of the products we produce.`,
+    category: "Design"
+  },
+  {
+    id: 1,
+    title: "Front End Jr.Developer",
+    location: "United States",
+    remote: true,
+    paymentVerified: true,
+    hourlyUSD: 30,
+    postedTime: "50 minutes ago",
+    description: `Developing and testing UI for web applications.
+    Transforming UI designs and mockups into pages and flows using CSS3 and JavaScript.
+    Building modular front-end code leveraging REST-based services.
+    Developing tests to ensure product quality.`,
+    category: "Front End Dev"
+  }
+];
+
+export default JOBS;
